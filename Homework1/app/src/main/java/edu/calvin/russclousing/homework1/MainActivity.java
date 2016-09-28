4541e0c529ced4a1fbd324be094f3b231ab124bb
Russ Clousing <russell.clousingiii@gmail.com>
2016-09-28 16:36:54 -0400

    finished homework1

--- Homework1/app/src/main/java/edu/calvin/russclousing/homework1/MainActivity.java
+++ Homework1/app/src/main/java/edu/calvin/russclousing/homework1/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.EditText;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor.*;
 
 public class MainActivity extends AppCompatActivity {
     int value1;//all of the needed variables for switching types
@@ -17,12 +19,15 @@ public class MainActivity extends AppCompatActivity {
     String value1string;
     String value2string;
     String resultstring;
-
+    private SharedPreferences savedValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+
         final EditText value1Text = (EditText) findViewById(R.id.editText);//connecting text fields to variables
         final EditText value2Text = (EditText) findViewById(R.id.editText2);
         final TextView resultText = (TextView) findViewById(R.id.textView4);
@@ -43,42 +48,56 @@ public class MainActivity extends AppCompatActivity {
                 value1string = value1Text.getText().toString();
                 value2string = value2Text.getText().toString();
 
-                if(value1string.equals("")){//convert the strings of value1 and value2 to ints
+                if (value1string.equals("")) {//convert the strings of value1 and value2 to ints
                     value1 = 0;
-                }
-                else {
+                } else {
                     value1 = Integer.parseInt(value1Text.getText().toString());
                 }
-                if(value2string.equals("")){
+                if (value2string.equals("")) {
                     value2 = 0;
                 } else {
                     value2 = Integer.parseInt(value2Text.getText().toString());
                 }
                 position = operator.getSelectedItemPosition();//gets the correct operator
-                if(position == 0){//addition
+                if (position == 0) {//addition
                     result = value1 + value2;
                     resultstring = String.valueOf(result);
-                }
-                else if(position == 1){//subtraction
+                } else if (position == 1) {//subtraction
                     result = value1 - value2;
                     resultstring = String.valueOf(result);
-                }
-                else if(position == 2){//multiplicaiton
+                } else if (position == 2) {//multiplicaiton
                     result = value1 * value2;
                     resultstring = String.valueOf(result);
-                }
-                else if(position == 3 && value2 != 0 ){//division preventing divede by zero
+                } else if (position == 3 && value2 != 0) {//division preventing divede by zero
                     result = value1 / value2;
                     resultstring = String.valueOf(result);
-                }
-                else if(value2 == 0){//error msg for divide by zero
+                } else if (value2 == 0) {//error msg for divide by zero
                     resultstring = "cannot divide by zero";
                 }
 
                 resultText.setText(resultstring);//changing the result on the app
             }//this is end of onclick
         });
-
-
     }
+    //overide method for on pause. stores user input that will be restored in the onresume method
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putInt("value one", value1);
+        editor.putInt("value two", value2);
+        editor.putInt("operator value", position);
+        editor.commit();
+    }
+    //override for onresume. pulls stored values and restores them when the app is active again after being paused.
+    @Override
+    public void onResume() {
+        super.onResume();
+        value1 = savedValues.getInt("value one", value1);
+        value2 = savedValues.getInt("value two", value2);
+        position = savedValues.getInt("operator value", position);
+    }
+
 }
+
+
